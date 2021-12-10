f285d7916cb5d716e5cb27a39d8facb171e69107
anonymous anonymous <liz.stanfel@kellogg.northwestern.edu>
2021-12-10 05:58:24 +0000

    Direct association between users and entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :lists,
+             :class_name => "Entry",
+             :foreign_key => "creator_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:lists) }
+
     end
 
     describe "InDirect Associations" do
