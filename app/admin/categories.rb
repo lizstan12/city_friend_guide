606becfa76866e424383c2742e6619c55f8be2e1
anonymous anonymous <liz.stanfel@kellogg.northwestern.edu>
2021-12-10 05:58:36 +0000

    Whitelist columns in Category for ActiveAdmin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Category do
 
+ permit_params :art_design, :food, :historical
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
