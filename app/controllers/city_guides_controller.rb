5a832716a68b5c768d5f50c2ec14082d298eccf0
anonymous anonymous <liz.stanfel@kellogg.northwestern.edu>
2021-12-10 05:59:44 +0000

    Remove controller comments

--- app/controllers/city_guides_controller.rb
+++ app/controllers/city_guides_controller.rb
@@ -1,26 +1,21 @@
 class CityGuidesController < ApplicationController
   before_action :set_city_guide, only: %i[show edit update destroy]
 
-  # GET /city_guides
   def index
     @q = CityGuide.ransack(params[:q])
     @city_guides = @q.result(distinct: true).includes(:entries).page(params[:page]).per(10)
   end
 
-  # GET /city_guides/1
   def show
     @entry = Entry.new
   end
 
-  # GET /city_guides/new
   def new
     @city_guide = CityGuide.new
   end
 
-  # GET /city_guides/1/edit
   def edit; end
 
-  # POST /city_guides
   def create
     @city_guide = CityGuide.new(city_guide_params)
 
@@ -31,7 +26,6 @@ class CityGuidesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /city_guides/1
   def update
     if @city_guide.update(city_guide_params)
       redirect_to @city_guide, notice: "City guide was successfully updated."
@@ -40,7 +34,6 @@ class CityGuidesController < ApplicationController
     end
   end
 
-  # DELETE /city_guides/1
   def destroy
     @city_guide.destroy
     redirect_to city_guides_url,
@@ -49,12 +42,10 @@ class CityGuidesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_city_guide
     @city_guide = CityGuide.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def city_guide_params
     params.require(:city_guide).permit(:entry_id)
   end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,6 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: %i[show edit update destroy]
 
-  # GET /entries
   def index
     @q = Entry.ransack(params[:q])
     @entries = @q.result(distinct: true).includes(:creator, :category,
@@ -9,22 +8,17 @@ class EntriesController < ApplicationController
     @location_hash = Gmaps4rails.build_markers(@entries.where.not(geo_location_latitude: nil)) do |entry, marker|
       marker.lat entry.geo_location_latitude
       marker.lng entry.geo_location_longitude
-      marker.infowindow "<h5><a href='/entries/#{entry.id}'>#{entry.creator_id}</a></h5><small>#{entry.geo_location_formatted_address}</small>"
     end
   end
 
-  # GET /entries/1
   def show; end
 
-  # GET /entries/new
   def new
     @entry = Entry.new
   end
 
-  # GET /entries/1/edit
   def edit; end
 
-  # POST /entries
   def create
     @entry = Entry.new(entry_params)
 
@@ -40,7 +34,6 @@ class EntriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /entries/1
   def update
     if @entry.update(entry_params)
       redirect_to @entry, notice: "Entry was successfully updated."
@@ -49,7 +42,6 @@ class EntriesController < ApplicationController
     end
   end
 
-  # DELETE /entries/1
   def destroy
     @entry.destroy
     message = "Entry was successfully deleted."
@@ -62,12 +54,10 @@ class EntriesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_entry
     @entry = Entry.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def entry_params
     params.require(:entry).permit(:creator_id, :neighborhood, :cityguide_id,
                                   :geo_location, :category_id, :journal)
